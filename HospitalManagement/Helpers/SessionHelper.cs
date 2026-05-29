9fe292c9563c0f76edff413f333dc3be5eb41537
tuanbao205 <tuanbao050911@gmail.com>
2026-05-29 14:01:08 +0700

    Add BaseViewModel, RelayCommand, SessionHelper

---
+++ HospitalManagement/Helpers/SessionHelper.cs
@@ -0,0 +1,17 @@
+﻿using HospitalManagement.Models;
+
+namespace HospitalManagement.Helpers
+{
+    public static class SessionHelper
+    {
+        public static User CurrentUser { get; set; }
+
+        public static bool IsAdmin => CurrentUser?.Role == "Admin";
+        public static bool IsDoctor => CurrentUser?.Role == "Doctor";
+
+        public static void Logout()
+        {
+            CurrentUser = null;
+        }
+    }
+}
